ba246b50b09cb202384807f79d0c06fea9997a33
Mubarak Oladapo <oladapomubarak@gmail.com>
2023-09-05 16:11:46 +0100

    changed logut msg

--- controllers/authController.js
+++ controllers/authController.js
@@ -61,7 +61,7 @@ const logout = async (req, res) => {
     httpOnly: true,
     expires: new Date(Date.now() + 1000),
   });
-  res.status(StatusCodes.OK).json({ msg: "user logged out!" });
+  res.status(StatusCodes.OK).json({ msg: "user logged out successfuly!" });
 };
 
 module.exports = {
